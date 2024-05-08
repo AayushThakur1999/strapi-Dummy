f600824e0b9cdc2c940855c7b1fefb8bbcbe53f2
Aayush Thakur <ashcold21@gmail.com>
2024-05-08 17:26:59 +0530

    fix file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Hero from './Hero';
-import SubMenu from './SubMenu';
+import SubMenu from './Submenu';
 
 const App = () => {
 
